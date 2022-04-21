a705291abcdc72efeeda4f2839c46f70968ee8b5
Bouron Cédric <cedric.bouron@ynov.com>
2022-04-21 14:57:22 +0100

    ajout de truck

--- src/truck.py
+++ src/truck.py
@@ -1,51 +1,50 @@
 from shutil import move
 import time
 import threading
-from src import mapserver
+from mapserver import map_server
 
 class truck(threading.Thread):
 
-    def __init__(self, map, pos_x = 1, pos_y = 1):
+    def __init__(self, map:map_server, pos_x = 1, pos_y = 1):
         threading.Thread.__init__(self)
         self.x = pos_x
         self.y = pos_y
         self.score = 0
-        self.map = mapserver.map_server(map)
+        self.map = map
 
     def _move(self, value_x, value_y):
         self.x =value_x
         self.y =value_y
-    def check_nearby_crystol(self, map):
+
+    def check_nearby_crystol(self):
         value = 0
         move = [self.x+1,self.y]
-        if int(map[self.x+1][self.y])>value:
-            value = int(map[self.x+1][self.y])
+        if int(self.map.plan[self.x+1][self.y])>value:
+            value = int(self.map.plan[self.x+1][self.y])
             move = [self.x+1,self.y]
-        if int(map[self.x][self.y-1])>value:
-            value = int(map[self.x][self.y-1])
+        if int(self.map.plan[self.x][self.y-1])>value:
+            value = int(self.map.plan[self.x][self.y-1])
             move = [self.x,self.y-1]
-        if int(map[self.x-1][self.y])>value:
-            value = int(map[self.x-1][self.y])
+        if int(self.map.plan[self.x-1][self.y])>value:
+            value = int(self.map.plan[self.x-1][self.y])
             move = [self.x-1,self.y]
-        if int(map[self.x][self.y+1])>value:
-            value = int(map[self.x][self.y+1])
+        if int(self.map.plan[self.x][self.y+1])>value:
+            value = int(self.map.plan[self.x][self.y+1])
             move = [self.x,self.y+1]
-        self.score+=value
-
-        print("move : "+str(move[0]))
-        self.map.get_crystal(move[0], move[1])
+        self.score += value
+        self.map.get_crystal(move[0], move[1])     
         self._move(move[0], move[1])
 
-    def run(self, map):
-        while True:
-            self.check_nearby_crystol(map)
-            print("x : "+str(self.x)+", y : "+str(self.y))
-            time.sleep(1)
-            print(map.plan)
+    # def run(self):
+    #     while True:
+    #         print("aa")
+    #         self.check_nearby_crystol()
+    #         print("x : "+str(self.x)+", y : "+str(self.y))
+    #         time.sleep(1)
+    #         print(self.map.plan)
 
 
 
 if __name__ == "__main__":
     thread1 = truck(1)
     thread1.start()
-
