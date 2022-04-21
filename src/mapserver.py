ac926ace6472ed1e0cb1fb7a9f2a5dd272cf7890
Bouron Cédric <cedric.bouron@ynov.com>
2022-04-21 14:52:58 +0100

    apprends à coder clément

--- src/mapserver.py
+++ src/mapserver.py
@@ -1,18 +1,15 @@
 import time
-
 class map_server():
     def __init__(self, plana):
         self.plan = plana
         self.is_digging = False
 
+    def display(self):
+        for ligne in self.plan:
+            for char in ligne:
+                print(char+" ", end = '')
+            print("")
+
     def get_crystal(self, x, y):
-        while True:
-            if self.is_digging == False:
-                self.is_digging = True
-                break
-        print(self.plan)
-        print("aaaaaaaaaaaaaaaaaaaaaaaaa")
         self.plan[x][y] = '0'
-        
-
-
+        self.is_digging = False
