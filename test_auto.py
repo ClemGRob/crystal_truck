da5c3be379abfd1545ae7ec9d2ec259dc9983ea1
Bouron Cédric <cedric.bouron@ynov.com>
2022-04-21 12:01:58 +0100

    test1

--- test_auto.py
+++ test_auto.py
@@ -1,8 +1,5 @@
-import game
-import sys
-import io
 import pytest
-from contextlib import redirect_stdout
+
 
 def func(x):
     return x + 1
@@ -11,27 +8,3 @@ def test_count():
     assert func(3) == 4
 
 
-def get_ma_map():
-    f = io.StringIO()
-    with redirect_stdout(f):
-        game.init_game(2)
-    out = f.getvalue()
-    lignes = out.split('\n')
-    return lignes[5:-4]
-
-# def get_map(map = "map.txt"):
-#     f = open(map, "r")
-#     carte = f.read()
-#     lignes = carte.split('\n')
-#     crystaux = []
-#     i = 0
-#     for ligne in lignes:
-#         crystaux.insert(i, [char for char in ligne] )
-#         i+=1
-#     print(crystaux[1][1])
-#     print(crystaux[1][2])
-#     print(crystaux[1][3])
-#     print(crystaux[1][4])
-        
-
-get_ma_map()
