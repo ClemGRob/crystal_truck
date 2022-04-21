bce6a658bb61044de1d8b9b20d62b3c3bc649e34
Bouron Cédric <cedric.bouron@ynov.com>
2022-04-21 14:19:07 +0100

    correction test

--- test_auto.py
+++ test_auto.py
@@ -1,8 +1,8 @@
 import pytest
-import src.game
+import game
 
 def test_get_map():
-    result = src.main.get_map()
+    result = game.get_map()
     assert isinstance(result, list)
     assert result
 
