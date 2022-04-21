d1f97648827408844de1de661e69685a9f13b6d8
Bouron Cédric <cedric.bouron@ynov.com>
2022-04-21 15:47:30 +0100

    new

--- test_auto.py
+++ test_auto.py
@@ -1,17 +1,8 @@
-from asyncio.windows_events import NULL
-import game
-import sys
-import io
 import pytest
-import src.main
-from contextlib import redirect_stdout
-
-
-
-
+from src import main
 
 def test_get_map():
-    result = src.main.get_map()
+    result = main.get_map()
     assert isinstance(result, list)
     assert result
 
