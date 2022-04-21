f19ff136ac1d41cc24ada6921d2befcd2fa9df30
Bouron Cédric <cedric.bouron@ynov.com>
2022-04-21 16:15:04 +0100

    test_move

--- test/test_auto.py
+++ test/test_auto.py
@@ -1,10 +1,16 @@
+from shutil import move
+from typing_extensions import Self
 import pytest
-from src import main
+from src import main,truck
 
 def test_get_map():
     result = main.get_map()
     assert isinstance(result, list)
     assert result
 
-    
+def test_move():
+    truck1 = truck
+    truck1.move(3,3)
+    assert truck1.x == 3
+    assert truck1.y == 3
 
