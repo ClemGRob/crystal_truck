9fa0b1068079f7a70d3faa549b98afb96738dc36
Constantin CANETOS <70941287+canetos@users.noreply.github.com>
2022-04-22 12:45:55 +0200

    maj
    delete 'file.close

--- src/file_tool.py
+++ src/file_tool.py
@@ -2,7 +2,6 @@ def write_from_sratch(content, doc_name = "map_bis.txt"):
 	with open(doc_name, "w") as file:
 		# content = "%s" % out, "\n"
 		file.writelines(content)
-		file.close()
 
 
 
@@ -10,12 +9,10 @@ def write_new_line(content,doc_name = "map_bis.txt"):
 	with open(doc_name, "a") as file:
 		# content = "%s" % out, "\n"
 		file.writelines(content)
-		file.close()
 
 		
 def print_doc(doc_name = "map_bis.txt"):
 	with open(doc_name, "r") as file:
 		content = file.read()
 		print(content)
-		file.close()
 
