ee70deb7f9a29f2bd3551448b5f7a6d981dbd025
Bouron Cédric <cedric.bouron@ynov.com>
2022-04-22 08:53:10 +0100

    ajout test mouvement

--- test/test_auto.py
+++ test/test_auto.py
@@ -2,6 +2,7 @@ from shutil import move
 import pytest
 from src import main,truck
 
+
 def test_get_map():
     result = main.get_map()
     assert isinstance(result, list)
@@ -13,3 +14,22 @@ def test_move():
     assert truck1.x == 3
     assert truck1.y == 3
 
+def test_move_up():
+    truck1 = truck.truck(None,1,1)
+    truck1.move_up
+    assert truck1.y == 2
+
+def test_move_down():
+    truck1 = truck.truck(None,1,1)
+    truck1.move_down
+    assert truck1.y == 0
+
+def test_move_left():
+    truck1 = truck.truck(None,1,1)
+    truck1.move_left
+    assert truck1.x == 0
+
+def test_move_right():
+    truck1 = truck.truck(None,1,1)
+    truck1.move_right
+    assert truck1.x == 2
