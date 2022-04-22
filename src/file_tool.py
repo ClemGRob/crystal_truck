849afae601c91f9d2c53f9843311189410e3a4b2
Constantin <constantin.canetos@ynov.com>
2022-04-22 12:43:26 +0200

    maj file_tool

--- src/file_tool.py
+++ src/file_tool.py
@@ -2,20 +2,19 @@ def write_from_sratch(content, doc_name = "map_bis.txt"):
 	with open(doc_name, "w") as file:
 		# content = "%s" % out, "\n"
 		file.writelines(content)
-		file.close()
-
+		
 
 
 def write_new_line(content,doc_name = "map_bis.txt"):
 	with open(doc_name, "a") as file:
 		# content = "%s" % out, "\n"
 		file.writelines(content)
-		file.close()
+		
 
 		
 def print_doc(doc_name = "map_bis.txt"):
 	with open(doc_name, "r") as file:
 		content = file.read()
 		print(content)
-		file.close()
+		
 
