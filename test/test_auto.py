b9876c82ecc7434e8f9cef68b22ac5719472f945
Bouron Cédric <cedric.bouron@ynov.com>
2022-04-22 10:34:45 +0100

    ajout test abs_value

--- test/test_auto.py
+++ test/test_auto.py
@@ -2,6 +2,9 @@ from shutil import move
 import pytest
 from src import main,truck
 
+#truck
+truck1 = truck.truck(None,1,1)
+
 def test_init():
     map1 = [["0","2","0","0"],["0","0","0""0"]]
     truck1 =truck.truck(map1,10,10,10)
@@ -11,33 +14,32 @@ def test_init():
     assert truck1.id == 10
     assert truck1.map == map1
 
-def test_get_map():
-    result = main.get_map()
-    assert isinstance(result, list)
-    assert result
+def test_abs_value():
+    i = 3
+    truck1.abs_value(i)
+    assert i == 3
+    i == -5
+    truck1.abs_value(i)
+    assert i == 5
+
 
 def test_move_up():
-    truck1 = truck.truck(None,1,1)
     truck1.move_up()
     assert truck1.y == 2
 
 def test_move_down():
-    truck1 = truck.truck(None,1,1)
     truck1.move_down()
     assert truck1.y == 0
 
 def test_move_left():
-    truck1 = truck.truck(None,1,1)
     truck1.move_left()
     assert truck1.x == 0
 
 def test_move_right():
-    truck1 = truck.truck(None,1,1)
     truck1.move_right()
     assert truck1.x == 2
 
 def test_move():
-    truck1 = truck.truck(None,1,1)
     truck1._move(3,3)
     assert truck1.x == 3
     assert truck1.y == 3
@@ -47,9 +49,17 @@ def test_digg():
     truck1 = truck.truck(map,0,1)
     truck1.digg()
     truck1.digg()
-    assert truck1.map[0][1] == '0'
+    assert truck1.map[0][1] == '0'  
 
-def test_wright():
-    truck1 = truck.truck(None,1,1)
-    truck1.wright(action = "GOOD")
-    assert 
+#main
+def test_get_map():
+    result = main.get_map()
+    assert isinstance(result, list)
+    assert result
+
+
+
+#def test_wright():
+#   truck1 = truck.truck(None,1,1)
+#   truck1.wright(action = "GOOD")
+#   assert 
